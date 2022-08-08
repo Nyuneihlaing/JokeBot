69d02626eeba0fb19423bf650dbcac9accbf9f89
Nyun Ei Hlaing <nyuneihlaing1@gmail.com>
2022-08-08 09:42:20 -0700

    pyjokes

--- aca.py
+++ aca.py
@@ -1,26 +1,33 @@
 import logging
 import os
+import re
 
 from dotenv import load_dotenv
+import pyjokes
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-SLACK_BOT_TOKEN = os.environ["Bot_User_OAuth_Token"]
 SLACK_APP_TOKEN = os.environ["Socket_Mode_Token"]
+SLACK_BOT_TOKEN = os.environ["Bot_User_OAuth_Token"]
 
-app = App(token=SLACK_BOT_TOKEN)
+app = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
 
 @app.event("app_mention")
-def mention_handler(body, context, payload, options, say, event):
-    say("Hello World!")
+def event_test(event, say):
+    say(f"Hi there, <@{event['user']}>!")
 
-@app.event("message")
-def message_handler(body, context, paylbh5ad, options, say, event):
-    pass
+@app.message(re.compile("^joke$"))
+def show_random_joke(message, say):
+    dm_channel = message["channel"]
+
+    joke = pyjokes.get_joke()
+
+    say(text=joke, channel=dm_channel)  
+
 
 if __name__ == "__main__":
-    handler = SocketModeHandler(app,SLACK_APP_TOKEN)
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-viron.get("SLACK_BOT_TOKEN
+@app.event("message")
+# def message_handler(body, context, payload, options, say, event):
+#     say("hi")
